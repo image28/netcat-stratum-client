3075d829c0b11b7b9418b40b7150e8c8f9c9fa43
Kevin Macey <image28@slingshot.co.nz>
2020-12-05 11:50:01 +1300

    Updated connect.sh
    just testing some changes to mean.cpp that may allow the worker and the miner to work properly together without blocking.
    Will upload the changes to mean.cpp once the test has run correctly

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SERVER=""
-PORT=""
+SERVER="mwc.2miners.com"
+PORT="1111"
 
 # Logs into a stratum clinet and gets work
 function work()
@@ -8,21 +8,18 @@ function work()
 	DATE="`date +%Y.%j.%T`"
 	printf '%s' "$(cat connect.json | head -n2)" | netcat -v -i1 -q2 $SERVER $PORT > joblog-$DATE
 	
-	HEIGHT=`cat joblog-$DATE | grep -iEo "height\":[0-9]*" | grep -iEo "[0-9]*" | tail -n1`
-	NONCE=`hexdump -n 8 -e '4/4 "%08X" 1 "\n"' /dev/random | tr -d ' '`
-	JOBID=`cat joblog-$DATE | grep -iEo "job_id\":[0-9]*" | grep -iEo "[0-9]*" | tail -n1`
+	HEIGHT=`cat joblog-$DATE | grep -iEo "height\":[0-9]*" | grep -iEo "[0-9]*" | tail -n1 > .height`
+	NONCE=`hexdump -n 8 -e '4/4 "%08X" 1 "\n"' /dev/random | tr -d ' ' > .nonce`
+	JOBID=`cat joblog-$DATE | grep -iEo "job_id\":[0-9]*" | grep -iEo "[0-9]*" | tail -n1 > .id`
 	
 	echo "Working on jobid $JOBID on date $DATE at height $HEIGHT with nonce of $NONCE"
-	./mean31x8 -n $NONCE -a -t 8 -r 256 `cat joblog-$DATE | tail -n1 | rev | awk -F'"' '{print $2}' | rev` > worklog-$DATE &
-	sleep 4
-	 
-	tail -f "worklog-$DATE" | grep -E --line-buffered "^nonce" | rev | awk -F' ' '{print $1 "," $2 "," $3 "," $4}' | rev | while IFS=$'\n' read line; 
+	./mean31x8 -n $NONCE -a -t 8 -r 256 `cat joblog-$DATE | tail -n1 | rev | awk -F'"' '{print $2}' | rev` | while IFS=$'\n' read line; 
 	do
 		POW=`echo $line`
 		printf '%s' "$(cat connect.json)"| grep -v "getworktemplate" | netcat -v -i1 -q2 $SERVER $PORT
 		sleep 1
 		echo "Submitting work"
-		printf '%s' "$(cat submit.json | sed -E s/HEIGHT/$HEIGHT/ | sed -E s/JOBID/$JOBID/ | sed -E s/NONCE/$NONCE/ | sed -E s/POW/$POW/)" | netcat -v -i1 -q2 $SERVER $PORT;
+		printf '%s' "$(cat submit.json | sed -E s/HEIGHT/$HEIGHT/ | sed -E s/JOBID/$JOBID/ | sed -E s/NONCE/$NONCE/ | sed -E s/POW/$POW/)" #| netcat -v -i1 -q2 $SERVER $PORT;
 		pidof mean31x8
 		if test $? -ne 0
 		then 
